97d3ccc76eb694402946edba42179f138541ef2a
Pierre <pierre.sjoberg@kau.se>
2021-10-15 14:51:06 +0200

    WTF

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const cookieParser = require('cookie-parser');
-const io = require('socket.io')(http);
-const mymodule = require('./my-module.js');
-let que = [];
+//const express = require('express');
+//const app = express();
+//const http = require('http').createServer(app);
+//const cookieParser = require('cookie-parser');
+//const io = require('socket.io')(http);
+//const mymodule = require('./my-module.js');
+//let que = [];
 
 //app.use(cookieParser());
 //app.use(express.urlencoded({extended: true}));
@@ -18,3 +18,5 @@ app.get('/', function(req,res) {
         //res.sendFile(__dirname + '/index.html');     
 }); 
 */
+
+console.log('server is running on port ');
