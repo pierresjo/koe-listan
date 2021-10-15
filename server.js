d6a69ab1ad14c5a7e6a38ca99d7929338eff18a3
Pierre <pierre.sjoberg@kau.se>
2021-10-15 14:38:27 +0200

    Deleted evrything

--- server.js
+++ server.js
@@ -16,88 +16,8 @@ let server = http.listen(80, () => {
 
 app.get('/', function(req,res) {
 
-    let cookie = req.cookies.nickName;
 
-    if(cookie==null) {
-        res.sendFile(__dirname + '/loggain.html');
-    }
-    else if(cookie==='secretAdmin') {
-        res.sendFile(__dirname + '/secretAdmin.html');
-        
-    }
-    else {
         res.sendFile(__dirname + '/index.html');
         
-    }
-
-
+  
 }); 
-
-app.post('/', function (req,res) {
-    let cookie = req.cookies.nickName;
-
-    if(cookie==null) {
-        res.cookie('nickName', req.body.nickname, { maxAge: 1000*60*60*24*31, httpOnly: false});
-    }
-
-    res.redirect('/');
-
-});
-
-app.get('/favicon.ico', function(req,res) {
-    res.sendFile(__dirname + '/favicon.ico');
-});
-
-//Socketfunktioner....
-io.on('connection', function(socket) {
-
-    console.log('användare med id=' + socket.id + ' anslöt');
-
-    let cookieString = socket.handshake.headers.cookie;
-    let cookielist = mymodule.parseCookies(cookieString);
-
-    if(cookielist.nickName!=null) {
-        socket.nickName = cookielist.nickName;        
-    }
-
-    io.emit('updateList', que);
-
-    socket.on('addGroup', function(data) {
-        //let time = mymodule.getTimeStamp();
-        //io.emit('changeBackground', {"imageid" : data.backgroundId, "time": time, "name": socket.nickName});
-
-        //Kolla om redan i kö... annars
-        let pos = { grupp: socket.nickName, msg: data.msg};    
-        que.push(pos);
-
-        io.emit('updateList', que);
-      
-    });
-
-    socket.on('deleteGroup', function (data) {
-
-        //Ta bort ur kö och uppdatera
-
-        for(let i=0; i<que.length; i++) {
-            if(que[i].grupp === data.grupp) {
-                que.splice(i,1);
-            }
-        }
-
-        io.emit('updateList', que);
-
-    });
-
-    socket.on('disconnect', ()=> {
-        console.log(socket.nickName + ' disconnected');
-    });
-
-
-});
-
-app.use('/public', express.static(__dirname + '/public'));
-
-app.use(function(req,res) {
-    res.status(404);
-    res.send('<h1>404 Page not found</h1><p>Dumkopf</p>');
-});
